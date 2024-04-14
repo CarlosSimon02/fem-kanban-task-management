6366902659b748491a95a21b48548b9d21c22366
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-15 06:28:06 +0800

    colors for color picker

---
+++ frontend/src/data/colors.ts
@@ -0,0 +1,23 @@
+export const materialColors = [
+  "#F44336", // Red
+  "#E91E63", // Pink
+  "#9C27B0", // Purple
+  "#8471F2", // LightPurple
+  "#673AB7", // DeepPurple
+  "#3F51B5", // Indigo
+  "#2196F3", // Blue
+  "#03A9F4", // LightBlue
+  "#49C4E5", // Cyan
+  "#009688", // Teal
+  "#4CAF50", // Green
+  "#67E2AE", // LightGreen
+  "#8BC34A", // YellowGreen
+  "#CDDC39", // Lime
+  "#FFEB3B", // Yellow
+  "#FFC107", // Amber
+  "#FF9800", // Orange
+  "#FF5722", // DeepOrange
+  "#795548", // Brown
+  "#9E9E9E", // Grey
+  "#607D8B", // BlueGrey
+];
