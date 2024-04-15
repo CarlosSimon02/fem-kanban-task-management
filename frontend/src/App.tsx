e1cac2f81bacb40d6fb02262a17e54ab815c11dc
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-16 06:30:25 +0800

    initialize data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { boards as dataBoards } from "@/data/data.json";
+import { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import router from "./router";
+import { useBoardStore } from "./store/boardStore";
+import { useThemeStore } from "./store/themeStore";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const theme = useThemeStore((state) => state.theme);
+  const systemTheme = useThemeStore((state) => state.systemTheme);
+  const setBoards = useBoardStore((state) => state.setBoards);
+  const setCurrentBoardIndex = useBoardStore(
+    (state) => state.setCurrentBoardIndex,
+  );
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(() => {
+    const root = window.document.documentElement;
 
-export default App
+    root.classList.remove("light", "dark");
+
+    if (theme === "system") {
+      root.classList.add(systemTheme as string);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme, systemTheme]);
+
+  useEffect(() => {
+    setBoards(
+      dataBoards.map((board) => ({
+        ...board,
+        id: uuidv4(),
+        columns: board.columns.map((column) => ({
+          ...column,
+          id: uuidv4(),
+          tasks: column.tasks.map((task) => ({
+            ...task,
+            id: uuidv4(),
+            subtasks: task.subtasks.map((subtask) => ({
+              ...subtask,
+              id: uuidv4(),
+            })),
+          })),
+        })),
+      })),
+    );
+
+    setCurrentBoardIndex(0);
+  }, [setBoards, setCurrentBoardIndex]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
