d51c5b48e4fc5e6d54ab61affc82eb54475b54b5
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-17 06:32:37 +0800

    created component

---
+++ frontend/src/components/CreateNewBoard.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import BoardDialog from "./dialogs/BoardDialog";
+import { DialogTrigger } from "./ui/Dialog";
+import { BoardIcon } from "./ui/Icons";
+
+type CreateNewBoardProps = {
+  className?: string;
+};
+
+const CreateNewBoard = ({ className }: CreateNewBoardProps) => {
+  return (
+    <div className="pr-6">
+      <BoardDialog>
+        <DialogTrigger asChild>
+          <button
+            className={cn(
+              "clickable group mt-[0.125rem] flex w-full items-center gap-3 rounded-r-3xl px-6 py-[0.875rem] text-[0.9375rem] font-bold text-accent hover:text-accent-hover lg:px-8",
+              className,
+            )}
+          >
+            <BoardIcon className="w-4 [&_path]:fill-accent group-hover:[&_path]:fill-accent-hover" />
+            <div>
+              <span aria-hidden={true}>+</span> Create New Board
+            </div>
+          </button>
+        </DialogTrigger>
+      </BoardDialog>
+    </div>
+  );
+};
+
+export default CreateNewBoard;
