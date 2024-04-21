64a1bf1485f35d2589838230e52276a31ce302f9
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-22 06:35:58 +0800

    created form

---
+++ frontend/src/forms/columnForm/ColumnForm.tsx
@@ -0,0 +1,85 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormTag,
+} from "@/components/ui/Form";
+import { Input } from "@/components/ui/Input";
+import { getRandomColor } from "@/lib/utils";
+import { useBoardStore } from "@/store/boardStore";
+import { ColumnType } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+import ColorPicker from "../ColorPicker";
+import { columnFormSchema } from "../schemas";
+
+type ColumnFormType = {
+  columnToEdit?: ColumnType; //if undefined, it means form is on "Create New Board" phase
+  afterSave?: () => void;
+};
+
+type ColumnData = z.infer<typeof columnFormSchema>;
+
+const ColumnForm = ({ columnToEdit, afterSave }: ColumnFormType) => {
+  const updateColToCurrentBoard = useBoardStore(
+    (state) => state.updateColToCurrentBoard,
+  );
+  const addColToCurrentBoard = useBoardStore(
+    (state) => state.addColToCurrentBoard,
+  );
+  const form = useForm<ColumnData>({
+    resolver: zodResolver(columnFormSchema),
+    defaultValues: columnToEdit || {
+      id: uuidv4(),
+      color: getRandomColor(),
+      name: "",
+      tasks: [],
+    },
+  });
+
+  const onSave = (colData: ColumnData) => {
+    if (columnToEdit) {
+      updateColToCurrentBoard(colData as unknown as ColumnType);
+    } else {
+      addColToCurrentBoard(colData as unknown as ColumnType);
+    }
+
+    afterSave && afterSave();
+  };
+
+  return (
+    <Form {...form}>
+      <FormTag onSubmit={form.handleSubmit(onSave)}>
+        <div className="flex items-start gap-4">
+          <FormField
+            name="color"
+            render={({ field }) => <ColorPicker name="color" field={field} />}
+          />
+          <FormField
+            name="name"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormControl>
+                  <Input placeholder="e.g. To Do" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <button
+          type="submit"
+          className="clickable flex items-center justify-center rounded-full bg-accent p-2 text-sm font-bold text-accent-foreground hover:bg-accent-hover"
+        >
+          {columnToEdit ? "Save Changes" : "Create New Board"}
+        </button>
+      </FormTag>
+    </Form>
+  );
+};
+
+export default ColumnForm;
