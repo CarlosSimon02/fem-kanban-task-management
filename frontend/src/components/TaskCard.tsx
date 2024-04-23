039186b81833d7d6e47b86f68fd4aec18f46cfc4
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-23 17:53:01 +0800

    implement task dialog when task card is clicked

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -1,5 +1,7 @@
 import { TaskType } from "@/types";
 import { Draggable } from "react-beautiful-dnd";
+import TaskDialog from "./dialogs/TaskDialog";
+import { TaskOptionsContextMenu } from "./dialogs/TaskOptions";
 import { Progress } from "./ui/Progress";
 
 type TaskCardProps = {
@@ -17,25 +19,28 @@ const TaskCard = ({ data, themeColor, index }: TaskCardProps) => {
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
-        <li
-          ref={provided.innerRef}
-          className="card mb-5 w-full rounded-lg bg-card px-4 py-6 text-left text-card-foreground shadow-card-shadow"
-          onClick={() => alert("Item Clicked")}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <div className="text-[0.9375rem] font-bold text-primary-foreground">
-            {data.title}
-          </div>
-          <div className="mt-5 text-xs font-bold">
-            <Progress
-              value={(completedSubtasksCount / subTasksCount) * 100}
-              color={themeColor}
-              className="mb-1"
-            />
-            {completedSubtasksCount} of {subTasksCount} subtasks
-          </div>
-        </li>
+        <TaskOptionsContextMenu task={data}>
+          <TaskDialog task={data}>
+            <li
+              ref={provided.innerRef}
+              className="card mb-5 w-full rounded-lg bg-card px-4 py-6 text-left text-card-foreground shadow-card-shadow"
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              <div className="text-[0.9375rem] font-bold text-primary-foreground">
+                {data.title}
+              </div>
+              <div className="mt-5 text-xs font-bold">
+                <Progress
+                  value={(completedSubtasksCount / subTasksCount) * 100}
+                  color={themeColor}
+                  className="mb-1"
+                />
+                {completedSubtasksCount} of {subTasksCount} subtasks
+              </div>
+            </li>
+          </TaskDialog>
+        </TaskOptionsContextMenu>
       )}
     </Draggable>
   );
