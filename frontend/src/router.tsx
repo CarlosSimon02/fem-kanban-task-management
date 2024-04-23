d2b1f36fd6743e4bcaf2b6820572698ceb6b4113
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-23 17:50:30 +0800

    movedd element of path / to a seperate component

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,20 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Board from "./components/Board";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import Root from "./Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <>
-        <Header />
-        <div className="flex flex-1 items-stretch">
-          <Sidebar />
-          <Board />
-        </div>
-      </>
-    ),
+    element: <Root />,
   },
 ]);
 
