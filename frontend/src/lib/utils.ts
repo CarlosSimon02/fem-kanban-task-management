83f414980015b6029854a707ffb2f6d5072fb94c
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-24 17:57:01 +0800

    implement function to find task in the current board

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { materialColors } from "@/data/colors";
+import { BoardType } from "@/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -9,4 +10,28 @@ export const cn = (...inputs: ClassValue[]) => {
 export const getRandomColor = () => {
   const randomIndex = Math.floor(Math.random() * materialColors.length);
   return materialColors[randomIndex];
+};
+
+export const findTaskPosition = (
+  currentBoard: BoardType,
+  taskToUpdateId: string,
+) => {
+  let taskToUpdatePos = { col: -1, row: -1 };
+
+  for (let col = 0; col < currentBoard.columns.length; col++) {
+    let found = false;
+
+    for (let row = 0; row < currentBoard.columns[col].tasks.length; row++) {
+      if (currentBoard.columns[col].tasks[row].id === taskToUpdateId) {
+        taskToUpdatePos = { col, row };
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+      break;
+    }
+  }
+
+  return taskToUpdatePos;
 };
