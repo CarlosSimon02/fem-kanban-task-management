8cd927e60b4649717e45abde6657a8896d3cab6f
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-25 07:22:12 +0800

    refactor

--- frontend/src/forms/schemas.ts
+++ frontend/src/forms/schemas.ts
@@ -2,19 +2,13 @@ import { z } from "zod";
 
 export const taskFormSchema = z.object({
   id: z.string(),
-  title: z
-    .string()
-    .transform((value) => value.replace(/\s+/g, ""))
-    .pipe(z.string().min(1, { message: "This field is required" })),
-  description: z.string().transform((value) => value.replace(/\s+/g, "")),
-  statusId: z.string(),
+  title: z.string().min(1, { message: "This field is required" }),
+  description: z.string().min(1, { message: "This field is required" }),
+  statusId: z.string().min(1, { message: "This field is required" }),
   subtasks: z.array(
     z.object({
       id: z.string(),
-      title: z
-        .string()
-        .transform((value) => value.replace(/\s+/g, ""))
-        .pipe(z.string().min(1, { message: "This field is required" })),
+      title: z.string().min(1, { message: "This field is required" }),
       isCompleted: z.boolean(),
     }),
   ),
@@ -23,23 +17,12 @@ export const taskFormSchema = z.object({
 export const columnFormSchema = z.object({
   id: z.string(),
   color: z.string(),
-  name: z
-    .string()
-    .transform((value) => value.replace(/\s+/g, ""))
-    .pipe(
-      z
-        .string()
-        .min(1, "This field is required")
-        .max(12, "Max characters is 12"),
-    ),
+  name: z.string().min(1, { message: "This field is required" }),
   tasks: z.array(taskFormSchema),
 });
 
 export const boardFormSchema = z.object({
   id: z.string(),
-  name: z
-    .string()
-    .transform((value) => value.replace(/\s+/g, ""))
-    .pipe(z.string().min(1, { message: "This field is required" })),
+  name: z.string().min(1, { message: "This field is required" }),
   columns: z.array(columnFormSchema),
 });
