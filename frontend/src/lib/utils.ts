ea80dc1e604966557d056ca1ed62724037510359
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-25 07:22:21 +0800

    refactor

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,5 +1,4 @@
 import { materialColors } from "@/data/colors";
-import { BoardType } from "@/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -10,28 +9,4 @@ export const cn = (...inputs: ClassValue[]) => {
 export const getRandomColor = () => {
   const randomIndex = Math.floor(Math.random() * materialColors.length);
   return materialColors[randomIndex];
-};
-
-export const findTaskPosition = (
-  currentBoard: BoardType,
-  taskToUpdateId: string,
-) => {
-  let taskToUpdatePos = { col: -1, row: -1 };
-
-  for (let col = 0; col < currentBoard.columns.length; col++) {
-    let found = false;
-
-    for (let row = 0; row < currentBoard.columns[col].tasks.length; row++) {
-      if (currentBoard.columns[col].tasks[row].id === taskToUpdateId) {
-        taskToUpdatePos = { col, row };
-        found = true;
-        break;
-      }
-    }
-    if (found) {
-      break;
-    }
-  }
-
-  return taskToUpdatePos;
 };
