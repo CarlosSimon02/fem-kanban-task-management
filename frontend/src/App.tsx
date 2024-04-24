68c237d53aa7dbd678bdd3142fd8efcc82495a47
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-25 07:19:36 +0800

    refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,20 @@
-import { boards as dataBoards } from "@/data/data.json";
 import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 import router from "./router";
 import { useBoardStore } from "./store/boardStore";
 import { useThemeStore } from "./store/themeStore";
 
 const App = () => {
-  const theme = useThemeStore((state) => state.theme);
-  const systemTheme = useThemeStore((state) => state.systemTheme);
-  const setBoards = useBoardStore((state) => state.setBoards);
+  const initThemeState = useThemeStore((state) => state.initThemeState);
+  const initBoardState = useBoardStore((state) => state.initBoardState);
 
   useEffect(() => {
-    const root = window.document.documentElement;
-
-    root.classList.remove("light", "dark");
-
-    if (theme === "system") {
-      root.classList.add(systemTheme as string);
-      return;
-    }
-
-    root.classList.add(theme);
-  }, [theme, systemTheme]);
+    initThemeState();
+  }, [initThemeState]);
 
   useEffect(() => {
-    setBoards(
-      dataBoards.map((board) => ({
-        ...board,
-        id: uuidv4(),
-        columns: board.columns.map((column) => {
-          const columnId = uuidv4();
-
-          return {
-            ...column,
-            id: columnId,
-            tasks: column.tasks.map((task) => ({
-              id: uuidv4(),
-              title: task.title,
-              description: task.description,
-              statusId: columnId,
-              subtasks: task.subtasks.map((subtask) => ({
-                ...subtask,
-                id: uuidv4(),
-              })),
-            })),
-          };
-        }),
-      })),
-    );
-  }, [setBoards]);
+    initBoardState();
+  }, [initBoardState]);
 
   return <RouterProvider router={router} />;
 };
