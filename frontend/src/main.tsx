5cbf60a028b52d129410222ba2a39607bb8ee2e9
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-25 07:19:58 +0800

    add Auth0ProviderWithNavigate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 import "./global.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Auth0ProviderWithNavigate>
+    <App />
+  </Auth0ProviderWithNavigate>,
+);
