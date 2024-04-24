75dec79b1d540a22ae13c4e11dea60740f9a062a
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-25 07:22:52 +0800

    add functions

--- frontend/src/store/themeStore.tsx
+++ frontend/src/store/themeStore.tsx
@@ -1,29 +1,38 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
-type Theme = "dark" | "light" | "system";
+type Theme = "dark" | "light";
+
+const setRootElementTheme = (theme: Theme) => {
+  const root = window.document.documentElement;
+  root.classList.remove("light", "dark");
+  root.classList.add(theme);
+};
+
+const getSystemTheme = () => {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+};
 
 type ThemeStore = {
-  systemTheme: Omit<Theme, "system">;
   theme: Theme;
+  initThemeState: () => void;
   setTheme: (theme: Theme) => void;
 };
 
-export const useThemeStore = create<ThemeStore>()(
-  persist(
-    (set) => ({
-      systemTheme: (() => {
-        return window.matchMedia("(prefers-color-scheme: dark)").matches
-          ? "dark"
-          : "light";
-      })(),
-      theme: "system",
-      setTheme: (theme: Theme) => {
-        set({ theme });
-      },
-    }),
-    {
-      name: "vite-ui-theme",
-    },
-  ),
-);
+export const useThemeStore = create<ThemeStore>((set) => ({
+  theme: "light",
+  initThemeState: () => {
+    const storedTheme = localStorage.getItem("vite-ui-theme");
+    const theme: Theme = storedTheme
+      ? (storedTheme as Theme)
+      : getSystemTheme();
+    setRootElementTheme(theme);
+    set({ theme });
+  },
+  setTheme: (theme: Theme) => {
+    localStorage.setItem("vite-ui-theme", theme);
+    setRootElementTheme(theme);
+    set({ theme });
+  },
+}));
