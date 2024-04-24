769cc1aeba86b27229e61dee6d47082b146b5c8a
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-25 07:20:35 +0800

    remove DialogTrigger

--- frontend/src/components/CreateNewBoard.tsx
+++ frontend/src/components/CreateNewBoard.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils";
 import BoardDialog from "./dialogs/BoardDialog";
-import { DialogTrigger } from "./ui/Dialog";
 import { BoardIcon } from "./ui/Icons";
 
 type CreateNewBoardProps = {
@@ -10,20 +9,18 @@ type CreateNewBoardProps = {
 const CreateNewBoard = ({ className }: CreateNewBoardProps) => {
   return (
     <div className="pr-6">
-      <BoardDialog>
-        <DialogTrigger asChild>
-          <button
-            className={cn(
-              "clickable group mt-[0.125rem] flex w-full items-center gap-3 rounded-r-3xl px-6 py-[0.875rem] text-[0.9375rem] font-bold text-accent hover:text-accent-hover lg:px-8",
-              className,
-            )}
-          >
-            <BoardIcon className="w-4 [&_path]:fill-accent group-hover:[&_path]:fill-accent-hover" />
-            <div>
-              <span aria-hidden={true}>+</span> Create New Board
-            </div>
-          </button>
-        </DialogTrigger>
+      <BoardDialog >
+        <button
+          className={cn(
+            "clickable group mt-[0.125rem] flex w-full items-center gap-3 rounded-r-3xl px-6 py-[0.875rem] text-[0.9375rem] font-bold text-accent hover:text-accent-hover lg:px-8",
+            className,
+          )}
+        >
+          <BoardIcon className="w-4 [&_path]:fill-accent group-hover:[&_path]:fill-accent-hover" />
+          <div>
+            <span aria-hidden={true}>+</span> Create New Board
+          </div>
+        </button>
       </BoardDialog>
     </div>
   );
