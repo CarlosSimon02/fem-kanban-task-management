3e35114b69d9f28de83858f83c52dab7d53b3580
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-27 15:43:18 +0800

    render the application with nested providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
 import "./global.css";
+import Auth0ProviderWithNavigate from "./providers/Auth0ProviderWithNavigate";
+import LoadDataProvider from "./providers/LoadDataProvider";
+import PersistDataProvider from "./providers/PersistDataProvider";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Auth0ProviderWithNavigate>
-    <App />
-  </Auth0ProviderWithNavigate>,
+  <Router>
+    <QueryClientProvider client={queryClient}>
+      <Auth0ProviderWithNavigate>
+        <PersistDataProvider>
+          <LoadDataProvider>
+            <AppRoutes />
+          </LoadDataProvider>
+        </PersistDataProvider>
+      </Auth0ProviderWithNavigate>
+    </QueryClientProvider>
+  </Router>,
 );
