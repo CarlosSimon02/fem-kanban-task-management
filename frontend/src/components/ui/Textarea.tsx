e9a4ff162441ffa3acae83ac347fe44a2fb242cc
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-28 15:53:49 +0800

    update styles

--- frontend/src/components/ui/Textarea.tsx
+++ frontend/src/components/ui/Textarea.tsx
@@ -12,7 +12,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex h-[7rem] w-full rounded-[0.25rem] border-[0.09375rem] border-input bg-primary px-4 py-2 text-sm text-primary-foreground placeholder:text-primary-foreground placeholder:opacity-25 focus-visible:border-accent focus-visible:!outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-[7rem] min-h-[7rem] w-full rounded-[0.25rem] border-[0.09375rem] border-input bg-primary px-4 py-2 text-sm text-primary-foreground placeholder:text-primary-foreground placeholder:opacity-25 focus-visible:border-accent focus-visible:!outline-none disabled:cursor-not-allowed disabled:opacity-50",
           error && "!border-destructive caret-destructive",
           className,
         )}
