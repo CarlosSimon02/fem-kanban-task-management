31b0f68b645199af62b4c729f7606de7d1f74697
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-28 15:52:50 +0800

    create auth callback page

---
+++ frontend/src/pages/AuthCallbackPage.tsx
@@ -0,0 +1,33 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthCallbackPage = () => {
+  const navigate = useNavigate();
+  const { user } = useAuth0();
+  const { createUser } = useCreateMyUser();
+
+  const hasCreatedUser = useRef(false);
+
+  useEffect(() => {
+    if (
+      user?.sub &&
+      user?.given_name &&
+      user?.picture &&
+      !hasCreatedUser.current
+    ) {
+      createUser({
+        auth0Id: user.sub,
+        name: user.given_name,
+        picture: user.picture,
+      });
+      hasCreatedUser.current = true;
+    }
+    navigate("/");
+  }, [createUser, navigate, user]);
+
+  return <>Loading...</>;
+};
+
+export default AuthCallbackPage;
