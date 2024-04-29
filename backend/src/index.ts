8f85278ad0fd39d84d938f7655ba40c5dea69543
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-29 19:50:49 +0800

    add api routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,9 @@ import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import myBoardsRoute from "./routes/MyBoardsRoute";
+import myColumnsRoute from "./routes/MyColumnsRoute";
 import myCurrentBoardIndexRoute from "./routes/MyCurrentBoardIndexRoute";
+import myTasksRoute from "./routes/MyTasksRoute";
 import myThemeRoute from "./routes/MyThemeRoute";
 import myUserRoute from "./routes/MyUserRoute";
 
@@ -19,8 +21,8 @@ app.use("/api/my/user", myUserRoute);
 app.use("/api/my/theme", myThemeRoute);
 app.use("/api/my/current-board-index", myCurrentBoardIndexRoute);
 app.use("/api/my/boards", myBoardsRoute);
-// app.use("/api/my/column", myColumnRoute);
-// app.use("/api/my/task", myTaskRoute);
+app.use("/api/my/column", myColumnsRoute);
+app.use("/api/my/task", myTasksRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
