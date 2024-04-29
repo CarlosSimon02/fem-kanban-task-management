bf851b61ddf6a6d04e5f5e2c06b1b7ce76fef322
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-29 19:56:10 +0800

    add board management routes to router

--- backend/src/routes/MyBoardsRoute.ts
+++ backend/src/routes/MyBoardsRoute.ts
@@ -5,5 +5,9 @@ import { jwtCheck, jwtParse } from "../middleware/auth";
 const router = express.Router();
 
 router.get("/", jwtCheck, jwtParse, MyBoardsController.getBoards);
+router.patch("/", jwtCheck, jwtParse, MyBoardsController.setBoards);
+router.patch("/board", jwtCheck, jwtParse, MyBoardsController.updateBoard);
+router.post("/board", jwtCheck, jwtParse, MyBoardsController.addBoard);
+router.delete("/board", jwtCheck, jwtParse, MyBoardsController.deleteBoard);
 
 export default router;
