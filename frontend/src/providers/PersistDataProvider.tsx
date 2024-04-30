ecd113f3dd03a4733a7b399a126595630cdb7770
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-30 20:07:02 +0800

    add welcome sonner

--- frontend/src/providers/PersistDataProvider.tsx
+++ frontend/src/providers/PersistDataProvider.tsx
@@ -2,7 +2,8 @@ import { useBoardStore } from "@/store/boardStore";
 import { useThemeStore } from "@/store/themeStore";
 import { BoardState } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
 type PersistDataProviderProps = {
   children: React.ReactNode;
@@ -14,26 +15,33 @@ const PersistDataProvider = ({ children }: PersistDataProviderProps) => {
   const boards = useBoardStore((state) => state.boards);
   const currentBoardIndex = useBoardStore((state) => state.currentBoardIndex);
 
-  const isNotInitRender = useRef(false);
-
   useEffect(() => {
-    if (!isAuthenticated && isNotInitRender) {
+    if (!isAuthenticated) {
       localStorage.setItem("vite-ui-theme", theme);
     }
-
-    isNotInitRender.current = true;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [theme]);
 
   useEffect(() => {
-    if (!isAuthenticated && isNotInitRender) {
+    if (!isAuthenticated) {
       const newBoardState: BoardState = { boards, currentBoardIndex };
       localStorage.setItem("board-state", JSON.stringify(newBoardState));
     }
-
-    isNotInitRender.current = true;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [boards, currentBoardIndex]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      toast("HI THERE!!", {
+        description: "You're now logged in and ready to access all features.",
+      });
+    } else {
+      toast("HI THERE!!", {
+        description:
+          "This web app utilizes 'localStorage' to store your data. Please log in to access your data across devices.",
+      });
+    }
+  }, [isAuthenticated]);
 
   return <>{children}</>;
 };
