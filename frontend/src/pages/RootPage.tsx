e213057b2665296feb5211908d3042ef43d27abf
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-30 20:06:32 +0800

    add something to display when there are no boards

--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -1,6 +1,8 @@
 import Board from "@/components/Board";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import BoardDialog from "@/components/dialogs/BoardDialog";
+import { EmptyBoardIcon } from "@/components/ui/Icons";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useBoardStore } from "../store/boardStore";
 import LoadingPage from "./LoadingPage";
@@ -18,7 +20,20 @@ const RootPage = () => {
       <Header />
       <div className="flex flex-1 items-stretch">
         <Sidebar />
-        {currentBoardIndexIsNumber && <Board />}
+        {currentBoardIndexIsNumber ? (
+          <Board />
+        ) : (
+          <div className="flex h-full flex-1 items-center justify-center">
+            <BoardDialog>
+              <button className="clickable group flex flex-col items-center gap-5 font-bold text-secondary-foreground hover:text-accent">
+                <EmptyBoardIcon className="[&_path]: h-32 w-32" />
+                <div>
+                  <span aria-hidden={true}>+</span> Create New Board
+                </div>
+              </button>
+            </BoardDialog>
+          </div>
+        )}
       </div>
     </>
   );
