541f7ae5de0c746103626f65d0f7dd50e2f6c8bd
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-30 20:02:24 +0800

    add toast errors

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -32,7 +33,14 @@ export const useCreateMyUser = () => {
     isLoading,
     isError,
     isSuccess,
+    error,
+    reset,
   } = useMutation(createMyUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
 
   return {
     createUser,
