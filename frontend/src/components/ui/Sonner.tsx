bc338695d1e8ec4b3fcc13f96b48004733a1ea24
CarlosSimon02 <caloycamacho02@gmail.com>
2024-04-30 20:05:27 +0800

    implement toaster component with dynamic theme support

---
+++ frontend/src/components/ui/Sonner.tsx
@@ -0,0 +1,29 @@
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group border toast group-[.toaster]:bg-primary-foreground group-[.toaster]:text-primary group-[.toaster]:border-border group-[.toaster]:shadow-2xl",
+          description: "group-[.toast]:text-primary/90",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
