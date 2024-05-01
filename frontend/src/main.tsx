2ee56784ee8020f18f23f47aef77ce7d17e38efd
CarlosSimon02 <caloycamacho02@gmail.com>
2024-05-01 21:08:37 +0800

    remove PersistDataProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,6 @@ import { Toaster } from "./components/ui/Sonner";
 import "./global.css";
 import Auth0ProviderWithNavigate from "./providers/Auth0ProviderWithNavigate";
 import LoadDataProvider from "./providers/LoadDataProvider";
-import PersistDataProvider from "./providers/PersistDataProvider";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,12 +19,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <Router>
     <QueryClientProvider client={queryClient}>
       <Auth0ProviderWithNavigate>
-        <PersistDataProvider>
-          <LoadDataProvider>
-            <AppRoutes />
-            <Toaster visibleToasts={1} position="bottom-right" richColors />
-          </LoadDataProvider>
-        </PersistDataProvider>
+        <LoadDataProvider>
+          <AppRoutes />
+          <Toaster visibleToasts={1} position="bottom-right" richColors />
+        </LoadDataProvider>
       </Auth0ProviderWithNavigate>
     </QueryClientProvider>
   </Router>,
